77fea3076ccf4523abbfc753729d3b105763af47
mahtab ansari <168529255+mahtabansa@users.noreply.github.com>
2026-07-20 12:22:15 +0530

    Change socket connection URL to production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:8000');
+const socket = io('https://online-stitching-app.onrender.com');
 
 socket.on('connect', () => {
   console.log('Socket connected:', socket.id);
