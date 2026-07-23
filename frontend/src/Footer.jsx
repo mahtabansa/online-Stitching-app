38f3f3ecc8e80c9ecbfcbcfc3349441badcb372f
md mahtab ansari <mdmahtab1334.in>
2026-07-23 08:38:25 +0530

    updated in Footer

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-       
-     <footer className=" bg-gray-500 text-white text-center  py-4 h-16 mt-4 " style={{width:'100%'}} >
-       © CopyRight ,contact At:9570197593 Email:mahtab957019@gmail.com
-     </footer>
-  
+
+    <footer className=" bg-gray-500 text-white text-center py-4 h-16 mt-4 " style={{ width: '100%' ,height:"100%" }} >
+      <span>© CopyRight ,contact At:9570197593 Email:mahtab957019@gmail.com</span>
+    </footer>
+
   )
 }
 
